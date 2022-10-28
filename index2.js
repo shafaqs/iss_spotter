a88fcdcaf1a08e2bc3392eae29e36a1fe21b6f9a
Shafaq Saud <shafaq.saud@gmail.com>
2022-10-28 17:51:58 -0400

    ptomise

--- index2.js
+++ index2.js
@@ -1,27 +1,4 @@
-// index.js;
-
-const { nextISSTimesForMyLocation } = require('./iss');
-
-nextISSTimesForMyLocation((error, passTimes) => {
-  if (error) {
-    return console.log("It didn't work!", error);
-  }
-  // success, print out the deets!
-  printPassTimes(passTimes);
-});
-
-
-/** 
- * Input: 
- *   Array of data objects defining the next fly-overs of the ISS.
- *   [ { risetime: <number>, duration: <number> }, ... ]
- * Returns: 
- *   undefined
- * Sideffect: 
- *   Console log messages to make that data more human readable.
- *   Example output:
- *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
- */
+const { nextISSTimesForMyLocation } = require('./iss_promised');
 const printPassTimes = function(passTimes) {
   for (const pass of passTimes) {
     const datetime = new Date(0);
@@ -30,4 +7,10 @@ const printPassTimes = function(passTimes) {
     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
   }
 };
-
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
